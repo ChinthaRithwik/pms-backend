6b3f7d924fad78c7a19fabca3406a69563b415e1
chintha rithwik <chintharithwik77@gmail.com>
2026-03-10 15:29:40 +0530

    fix: set timezone to Asia/Kolkata for correct IST timestamps

--- src/main/java/com/example/ProjectManagementSystem/ProjectManagementSystemApplication.java
+++ src/main/java/com/example/ProjectManagementSystem/ProjectManagementSystemApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectManagementSystemApplication.class, args);
-	}
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementSystemApplication.class, args);
+    }
 }
